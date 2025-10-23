b30ef37faf2d67225d81b5c098a3c17ac20bca1a
Christian <you@example.com>
2025-10-23 08:12:48 -0700

    chore: sync root and /docs, point API to Render, cache-bust (20251023081246)

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-﻿window.FB_API_URL    = "https://foodbridge-server-rv0a.onrender.com".replace(/\/$/, "");
+﻿window.FB_API_URL  = "https://foodbridge-server-rv0a.onrender.com";
 window.FB_PAGES_BASE = "/FoodBridge";
