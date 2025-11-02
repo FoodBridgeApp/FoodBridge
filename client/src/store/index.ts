e11ec2e410c95fea2a1c4e763b0bc198cd2698ba
castanedazen <149535722+castanedazen@users.noreply.github.com>
2025-11-02 01:40:41 -0700

    Create index.ts

---
+++ client/src/store/index.ts
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import cart from "./cartSlice";
+import session from "./sessionSlice";
+
+export const store = configureStore({
+  reducer: { cart, session },
+});
+
+store.subscribe(() => {
+  try { localStorage.setItem("fb.cart", JSON.stringify(store.getState().cart)); } catch {}
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
